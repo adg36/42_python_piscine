374f7f6631357d01b145aa9a3035a7576d929138
Renata <renata@example.com>
2026-01-27 17:55:00 +0000

    update

--- module03/ex0/ft_command_quest.py
+++ module03/ex0/ft_command_quest.py
@@ -2,18 +2,18 @@ import sys
 
 
 def ft_command_quest() -> None:
-    name = f"Program name: {sys.argv[0]}\n"
+    name = f"Program name: {sys.argv[0]}"
+    total = f"Total arguments: {len(sys.argv)}"
 
     print("=== Command Quest ===")
     if len(sys.argv) == 1:
-        print(f"No arguments provided!\n{name}"
-              f"Total arguments: {len(sys.argv)}")
+        print(f"No arguments provided!\n{name}\n{total}")
     else:
-        print(f"{name}"
+        print(f"{name}\n"
               f"Arguments received: {len(sys.argv) - 1}")
         for i in range(1, len(sys.argv)):
             print(f"Argument {i}: {sys.argv[i]}")
-        print(f"Total arguments: {len(sys.argv)}")
+        print(f"{total}")
 
 
 def main() -> None:
