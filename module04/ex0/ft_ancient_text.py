994fc4ccc1c6d902515b7a8f42af6f8aa2888a45
Renata <renata@example.com>
2026-02-03 15:02:10 +0000

    module04

--- module04/ex0/ft_ancient_text.py
+++ module04/ex0/ft_ancient_text.py
@@ -3,18 +3,18 @@
 def ft_ancient_text() -> None:
     print("=== CYBER ARCHIVES – DATA RECOVERY SYSTEM ===\n")
 
-    file = "ancient_fragment.txt"
-
-    print(f"Accessing Storage Vault: {file}")
-
+    filename = "ancient_fragment.txt"
+    print(f"Accessing Storage Vault: {filename}")
+    
     try:
-        with open(file) as f:
-            print("Connection established...\n"
-                  "\nRECOVERED DATA:")
-            print(f.read())
+        file = open("ancient_fragment.txt")
+        print("Connection established...\n"
+              "\nRECOVERED DATA:")
+        print(file.read())
     except FileNotFoundError:
         print("ERROR: Storage vault not found. Run data generator first.")
 
+    file.close()
     print("\nData recovery complete. Storage unit disconnected.")
 
 
---
+++ module04/ex4/ft_crisis_response.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+def ft_crisis_response() -> None:
+
+
+
+def main() -> None:
+    ft_crisis_response()
+
+
+if __name__ == "__main__":
+    main()
